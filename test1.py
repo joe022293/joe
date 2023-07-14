1b1400c02f720295370ad6d693ba651848052a1f
joe022293 <139582654+joe022293@users.noreply.github.com>
2023-07-15 05:41:56 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -185,7 +185,6 @@ try:
                 begin_list.append(begin)
                 final_list.append(final)
                 pdf_list.append(create_pdf(begin,final))
-                st.write(begin_list)
             elif '-' in i:
                 i_split=i.split('-')
                 if len(i_split) == 2 and i_split[0].isdigit() and len(i_split[0]) == 8 and i_split[1].isdigit() and len(i_split[1]) == 8:
@@ -196,8 +195,6 @@ try:
                     pdf_list.append(create_pdf(begin,final))
                 else:
                     st.write('error')
-        
-
         
             # Generate download link
         st.write(begin_list)
