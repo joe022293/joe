d3ffc9bbf3cb3ac16e18cf6749a0b0796aa5b1de
joe022293 <139582654+joe022293@users.noreply.github.com>
2023-07-15 04:54:52 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -137,20 +137,20 @@ try:
             i=i+1
     
         i = 0
-    
+        st.write('0001')
         while begin_num + i <= final_num and tp == 0:
             # 将PDF文件添加到合并器中
             merger.append(f"{begin_num+i}.pdf")
             i = i + 1
     
-    
+        st.write('0002')
         # 合并所有PDF文件
         merger_filename = f"{begin}to{final}.pdf"
         merger.write(merger_filename)
         merger.close()
         if begin_num!=0:
             print("PDF files merged successfully into", merger_filename)
-    
+        st.write('0003')
     
         i=0
         if begin_num!=0 and tp==0:
@@ -174,14 +174,14 @@ try:
     if st.button('confirm'):
         temp1=input.split(',')
         for i in temp1:
-            st.write(i)
+            #st.write(i)
             if len(i)==8 and i.isdigit():
                 begin=i
                 final=i
                 begin_list.append(begin)
                 final_list.append(final)
                 pdf_list.append(create_pdf(begin,final))
-                st.write(begin_list)
+                #st.write(begin_list)
             elif '-' in i:
                 i_split=i.split('-')
                 if len(i_split) == 2 and i_split[0].isdigit() and len(i_split[0]) == 8 and i_split[1].isdigit() and len(i_split[1]) == 8:
