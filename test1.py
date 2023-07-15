e9a82596829748401a53b9dcb263e4c2aeab5340
joe022293 <139582654+joe022293@users.noreply.github.com>
2023-07-15 13:55:01 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -16,8 +16,8 @@ try:
             os.remove(file_path)  # 删除文件
     
     
-    input = st.text_input("Registration QR codes(e.g. 42220001,44230010-44230020,45210001)")
-
+    input_i = st.text_input("Registration QR codes(e.g. 42220001,44230010-44230020,45210001)")
+    input=input_i.replace(" ", "")
     def create_pdf(begin,final):
 
         merger = PdfFileMerger()
