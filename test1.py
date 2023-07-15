e53a5b2e1d7822e27b264c9a1617d73ccdc756a7
joe022293 <139582654+joe022293@users.noreply.github.com>
2023-07-15 14:55:27 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -183,13 +183,13 @@ try:
             elif '-' in i:
                 i_split=i.split('-')
                 if len(i_split) == 2 and i_split[0].isdigit() and len(i_split[0]) == 8 and i_split[1].isdigit() and len(i_split[1]) == 8 and i_split[0][:4]==i_split[1][:4] and i_split[0][:2] in first_list:
-                    if int(i_split[1])-int(i_spit[0])<=200 and int(i_split[1])-int(i_spit[0])>0:
+                    if int(i_split[1])-int(i_split[0])<=200 and int(i_split[1])-int(i_split[0])>0:
                         begin=i_split[0]
                         final=i_split[1]
                         begin_list.append(begin)
                         final_list.append(final)
                         pdf_list.append(create_pdf(begin,final))
-                    elif int(i_split[0])-int(i_spit[1])<=200 and int(i_split[1])-int(i_spit[0])<=0:
+                    elif int(i_split[0])-int(i_split[1])<=200 and int(i_split[1])-int(i_split[0])<=0:
                         begin=i_split[1]
                         final=i_split[0]
                         begin_list.append(begin)
