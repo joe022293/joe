980117f43f2dfe345dfa0bd09d32a234a347156a
joe022293 <139582654+joe022293@users.noreply.github.com>
2024-11-28 18:47:51 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -99,7 +99,7 @@ try:
             )
             if mode==1:
                 data = f"www.medicapture.com/register/?serial={begin_num+i}"
-                if begin_prefix == '01' or begin_prefix == '02 or begin_prefix == '03 or begin_prefix == '04':
+                if begin_prefix == '01' or begin_prefix == '02' or begin_prefix == '03' or begin_prefix == '04':
                     data = f"www.medicapture.com/register/?serial=0{begin_num+i}"
                 else:
                     data = f"www.medicapture.com/register/?serial={begin_num+i}"
